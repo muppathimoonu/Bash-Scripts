3d76ce75b12134511700cee6e19f2c98eb2ecab5
33 <gogreenalways@live.com>
2016-05-16 00:33:02 +0530

    Replace colors builk..

--- Color-change.sh
+++ Color-change.sh
@@ -2,8 +2,8 @@
 # Script Written by 33
 totalfiles=(*)
 totalfiles=${#totalfiles[@]}
-replacewith='#4ad9f7'
-toreplace='#090909'
+replacewith='#4ad9f7' #(input your output color code here)
+toreplace='#090909' #(add the value of color which you want to replace in image)
 for i in $( ls ); do
 echo "Coloring: $i "
 mogrify $i -fuzz 100% -fill $replacewith -opaque $toreplace $i
